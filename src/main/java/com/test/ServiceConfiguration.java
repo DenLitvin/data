7f9c7ec569f25bea6a4e2617d623ce5eb0977480
Denis Ralkov <denis.ralkov@logmein.com>
2020-02-27 16:53:10 -0500

    Initial arrow flight server

--- src/main/java/com/test/ServiceConfiguration.java
+++ src/main/java/com/test/ServiceConfiguration.java
@@ -2,6 +2,10 @@ package com.test;
 
 import com.test.dao.IRecordDAO;
 import com.test.dao.RecordDaoCsv;
+import com.test.flight.ArrowFlightServer;
+import org.apache.arrow.flight.Location;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +16,13 @@ public class ServiceConfiguration {
     @Bean(name = "recordDaoCsv")
     public IRecordDAO recordDAO() {
         return new RecordDaoCsv();
+    }
+
+    @Bean
+    public ArrowFlightServer flightServer(){
+        final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+        return new ArrowFlightServer(allocator, Location.forGrpcInsecure("localhost", 31337));
+
     }
 
 
